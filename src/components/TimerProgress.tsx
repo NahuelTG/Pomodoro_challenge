98adee73cf7913f8e0b7ba9af7a6fd88d1dd540b
NahuelTG <nahueltorrezg@gmail.com>
2025-04-06 21:08:06 -0400

    style: progress bar

--- src/components/TimerProgress.tsx
+++ src/components/TimerProgress.tsx
@@ -1,12 +1,12 @@
+// src/components/TimerProgress.tsx
 import React from 'react'
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
-import type { TimerMode } from '../hooks/usePomodoro'
 
 interface Props {
   minutes: number
   seconds: number
-  mode: TimerMode
+  mode: 'pomodoro' | 'break'
 }
 
 export const TimerProgress: React.FC<Props> = ({ minutes, seconds, mode }) => {
@@ -15,20 +15,23 @@ export const TimerProgress: React.FC<Props> = ({ minutes, seconds, mode }) => {
   const percentage = (elapsedSeconds / totalSeconds) * 100
 
   const color = mode === 'pomodoro' ? '#E046D7' : '#3AB499'
+  const colorTrail = mode === 'pomodoro' ? '#3D244A' : '#1C3A3E'
 
   return (
-    <div className="contador__progress">
+    <>
       <CircularProgressbar
         value={percentage}
         text={`${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`}
         styles={buildStyles({
+          rotation: 0.25,
+          strokeLinecap: 'butt',
           textSize: '24px',
           pathTransitionDuration: 0.5,
           pathColor: color,
           textColor: color,
-          trailColor: color,
+          trailColor: colorTrail,
         })}
       />
-    </div>
+    </>
   )
 }
